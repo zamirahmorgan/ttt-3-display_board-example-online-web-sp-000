3a3dc30e1eb03cfe553d4dacf734daa4fc66d589
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-17 22:53:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "A Tic Tac Toe Board\n"
+  puts "A Tic Tac Toe Board"
   puts "   |   |   \n"
   puts "-----------\n"
   puts "   |   |  \n "
